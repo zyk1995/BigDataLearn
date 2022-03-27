fcd7b6c00d167b5fd66b083da9bc3b435d696d2e
zhangyikun <1251134350@qq.com>
2022-03-27 13:58:01 +0800

    hbase week3

--- Week_03/src/main/java/com/zyk/hbase/HbaseTest.java
+++ Week_03/src/main/java/com/zyk/hbase/HbaseTest.java
@@ -10,32 +10,24 @@ import java.io.IOException;
 
 public class HbaseTest {
 
+    /*
+     * docker run -d  -p 2181:2181 -p 16000:16000 -p 16010:16010 -p 16201:16201 -p 16301:16301 -p 8080:8080 -p 8085:8085 -p 9090:9090 -p:9095:9095 -p 16020:16020 --name hbase001 harisekhon/hbase /bin/sh
+     */
     public static void main(String[] args) throws IOException {
         // 建立连接
         Configuration configuration = HBaseConfiguration.create();
         configuration.set("hbase.zookeeper.quorum", "127.0.0.1");
         configuration.set("hbase.zookeeper.property.clientPort", "2181");
-        configuration.set("hbase.master", "127.0.0.1:55012");
+        configuration.set("hbase.master", "127.0.0.1:16000");
         Connection conn = ConnectionFactory.createConnection(configuration);
         Admin admin = conn.getAdmin();
 
-        TableName tableName = TableName.valueOf("zhangyikun:student");
+        TableName tableName = TableName.valueOf("student");
         String colFamilyInfo = "info";
         String colFamilyScore = "score";
 
         int rowKey = 1;
 
-        // 建表
-        if (admin.tableExists(tableName)) {
-            System.out.println("Table already exists");
-        } else {
-            HTableDescriptor hTableDescriptor = new HTableDescriptor(tableName);
-            HColumnDescriptor hColumnDescriptor = new HColumnDescriptor(colFamilyInfo);
-            hTableDescriptor.addFamily(hColumnDescriptor);
-
-            admin.createTable(hTableDescriptor);
-            System.out.println("Table create successful");
-        }
 
         // 插入数据
         Put put = new Put(Bytes.toBytes(rowKey)); // row key
